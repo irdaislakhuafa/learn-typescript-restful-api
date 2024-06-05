c91e00ac94fa25087cd1d92fe826be1cd7b824cc
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-06 01:58:02 +0700

    feat(application): add db utilities
    Add Prisma Client and configure it to log queries and events.

---
+++ src/application/db.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client/extension";
+import { log } from "./log";
+
+export const prismaClient = new PrismaClient({
+	log: [
+		{
+			emit: "event",
+			level: "query"
+		},
+		{
+			emit: "event",
+			level: "info"
+		},
+		{
+			emit: "event",
+			level: "warn"
+		},
+		{
+			emit: "event",
+			level: "error"
+		}
+	]
+});
+
+prismaClient.$on("query", async (e: any) => log.debug(e));
+prismaClient.$on("info", async (e: any) => log.info(e));
+prismaClient.$on("warn", async (e: any) => log.warn(e));
+prismaClient.$on("error", async (e: any) => log.error(e));
