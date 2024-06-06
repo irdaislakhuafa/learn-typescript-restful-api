b5157ef2bbd05c94f28d8c9d5d104deff5539ba0
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-06 14:10:25 +0700

    fix(validation): reorder return values
    Reorder the return values of the `validate` function to match the expected order of the response.
    
    This change was made to ensure that the response is consistent with the expected order of the response.

--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -3,12 +3,12 @@ import { Code } from "../code/code";
 import { ResponseError } from "../error/error";
 
 export class Validation {
-	static validate<T>(validate: ZodType, request: T): [T | null, Error | null] {
+	static validate<T>(validate: ZodType, request: T): [T, ResponseError | null] {
 		const result = validate.safeParse(request)
 		if (!result.success) {
 			const errs = result.error?.errors
 			const msg = errs.map((e): string => `${e.path.join(".")}: ${e.message.toLowerCase()}`).join(", ")
-			return [null, new ResponseError(Code.BAD_REQUEST, msg)]
+			return [request, new ResponseError(Code.BAD_REQUEST, msg)]
 		}
 
 		return [result.data, null]
