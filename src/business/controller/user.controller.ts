4495c85f47bfaeca4ef76bbaa82c5c0e4537c255
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-07 01:39:55 +0700

    feat: added login API
    This commit adds a new API endpoint for logging in users.
    
    The `POST /api/v1/auth/login` endpoint accepts a JSON payload with the following properties:
    
    - `username`: The username of the user attempting to log in.
    - `password`: The password of the user attempting to log in.
    
    If the credentials are valid, the endpoint will return a JSON Web Token (JWT) that can be used to authenticate the user in subsequent requests.

--- src/business/controller/user.controller.ts
+++ src/business/controller/user.controller.ts
@@ -1,7 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 import { Code } from "../../utils/code/code";
 import type { ResponseData } from "../model/generic.model";
-import type { RegisterUserRequest, UserResponse } from "../model/user.model";
+import type { LoginUserRequest, RegisterUserRequest, UserResponse } from "../model/user.model";
 import { UserService } from "../service/user.service";
 
 export class UserController {
@@ -10,6 +10,14 @@ export class UserController {
 			const result = await UserService.register(req.body as RegisterUserRequest)
 			const response: ResponseData<UserResponse> = { data: result }
 			res.status(Code.SUCCESS).json(response)
-		} catch (e) { next(e) }
+		} catch (err) { next(err) }
+	}
+
+	static async login(req: Request, res: Response, next: NextFunction) {
+		try {
+			const result = await UserService.login(req.body as LoginUserRequest)
+			const response: ResponseData<UserResponse> = { data: result }
+			res.status(Code.SUCCESS).json(response)
+		} catch (err) { next(err) }
 	}
 }
