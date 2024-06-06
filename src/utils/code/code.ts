81c5040a39e27f257530926241ad9bb6d5a0c454
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-07 01:36:08 +0700

    feat(user): add login feature
    Add login feature to the user service.
    
    BREAKING CHANGE: This change introduces a new field `token` to the `User` model.

--- src/utils/code/code.ts
+++ src/utils/code/code.ts
@@ -1,5 +1,6 @@
 export class Code {
 	static readonly SUCCESS: number = 200
 	static readonly BAD_REQUEST: number = 400
+	static readonly UNAUTHORIZED: number = 401
 	static readonly INTERNAL_SERVER_ERROR: number = 500
 }
