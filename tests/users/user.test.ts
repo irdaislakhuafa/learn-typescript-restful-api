b8347805e10430e632461a832df1c32b7835e1dd
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-06 18:14:32 +0700

    feat(users): add register test
    Adds a test for the register endpoint. This test checks that the endpoint:
    
    - Returns an error if the username already exists
    - Successfully registers a new user

--- tests/users/user.test.ts
+++ tests/users/user.test.ts
@@ -1,9 +1,10 @@
 import supertest from "supertest"
+import { v4 } from "uuid"
 import { log } from "../../src/application/log"
 import { web } from "../../src/application/web"
 import { Code } from "../../src/code/code"
 import { ResponseData } from "../../src/model/generic.model"
-import { RegisterUserRequest } from "../../src/model/user.model"
+import { RegisterUserRequest, UserResponse } from "../../src/model/user.model"
 
 describe("POST /api/v1/auth/register", () => {
 	it("must return error if username already exists", async () => {
@@ -35,5 +36,23 @@ describe("POST /api/v1/auth/register", () => {
 		log.debug(res.body)
 		expect(res.status).toBe(Code.BAD_REQUEST)
 		expect((res.body as ResponseData<any>).errors).toBeTruthy()
+	})
+
+	it("must success", async () => {
+		const username = `${v4()}`
+		const res = await supertest(web)
+			.post("/api/v1/auth/register")
+			.send({
+				name: "irda islakhu afa",
+				username: username,
+				password: "12345678"
+			} as RegisterUserRequest)
+
+		const body = (res.body as ResponseData<UserResponse>)
+		log.debug(res.body)
+		expect(res.status).toBe(Code.SUCCESS)
+		expect(body.errors).toBeFalsy()
+		expect(body.data?.name).toBe("irda islakhu afa")
+		expect(body.data?.username).toBe(username)
 	})
 })
