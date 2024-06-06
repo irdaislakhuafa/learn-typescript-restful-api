28e70d47f6e7593fe8a5e93ba7a67dc22bff00e7
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-07 02:26:28 +0700

    feat(auth): added login endpoint
    Added a new endpoint for user login and updated unit tests to cover the new functionality.

--- tests/users/user.test.ts
+++ tests/users/user.test.ts
@@ -2,12 +2,11 @@ import supertest from "supertest"
 import { log } from "../../src/application/log"
 import { web } from "../../src/application/web"
 import { ResponseData } from "../../src/business/model/generic.model"
-import { RegisterUserRequest, UserResponse } from "../../src/business/model/user.model"
+import { LoginUserRequest, RegisterUserRequest, UserResponse } from "../../src/business/model/user.model"
 import { Code } from "../../src/utils/code/code"
 import { UserUtil } from "./util.test"
 
 describe("POST /api/v1/auth/register", () => {
-	beforeEach(UserUtil.create)
 	afterEach(UserUtil.delete)
 
 	it("must return error if username already exists", async () => {
@@ -54,7 +53,29 @@ describe("POST /api/v1/auth/register", () => {
 		log.debug(res.body)
 		expect(res.status).toBe(Code.SUCCESS)
 		expect(body.errors).toBeFalsy()
-		expect(body.data?.name).toBe("irda islakhu afa")
+		expect(body.data?.name).toBe("test")
 		expect(body.data?.username).toBe("test")
+	})
+})
+
+describe("POST /api/v1/auth/login", () => {
+	beforeEach(UserUtil.create)
+	afterEach(UserUtil.delete)
+
+	it("login must success", async () => {
+		const res = await supertest(web)
+			.post("/api/v1/auth/login")
+			.send({
+				username: "test",
+				password: "test"
+			} as LoginUserRequest)
+
+		const body = (res.body as ResponseData<UserResponse>)
+
+		expect(res.status).toBe(Code.SUCCESS)
+		expect(body.data).toBeTruthy()
+		expect(body.data?.name).toBe("test")
+		expect(body.data?.username).toBe("test")
+		expect(body.data?.token).toBeDefined()
 	})
 })
