8f3b28672f4e3921bd56461efb9d369e37f08372
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-07 13:00:11 +0700

    feat: implement web server
    This commit implements a simple web server using Bun.

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log(`Hello via Bun!`);
+import { log } from "./src/application/log";
+import { web } from "./src/application/web";
+const port = Bun.env.APP_PORT || "3000"
+
+log.info(`server listening at port ${port}`)
+web.listen(Number(port))
