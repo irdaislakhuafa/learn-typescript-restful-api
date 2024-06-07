98fd6c7bd5824a35795c910af27973ba9a76db78
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-07 12:57:31 +0700

    feat: implement user controller
    Add user controller and corresponding API endpoint to get current user information.

--- src/route/auth.api.ts
+++ src/route/auth.api.ts
@@ -1,5 +1,7 @@
 import express from "express"
+import { UserController } from "../business/controller/user.controller"
 import { middlewareAuth } from "../middleware/auth"
 
 export const authRoute = express.Router()
 authRoute.use(middlewareAuth)
+authRoute.get("/api/v1/users/current", UserController.getCurrent)
