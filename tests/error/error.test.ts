a63d8f9f799f4e8aa16f0c99c39e00fe33a63bd6
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-07 16:44:28 +0700

    feat(error): add custom caller format message
    Adds a caller function to the ResponseError class, which allows users to customize the error message. This can be useful for providing more context or information about the error.
    
    BREAKING CHANGE: The error message format has changed. Previous versions of the library relied on the error's file and line properties to generate the error message. This format has been changed to use the caller function instead, which provides more flexibility and control over the error message.

--- tests/error/error.test.ts
+++ tests/error/error.test.ts
@@ -13,5 +13,26 @@ describe("error", () => {
 		expect(err.line).toBe("5")
 		expect(err.func).toBe('fn()')
 		expect(err.file).toBe(`${__filename}`)
+		expect(err.message).toBe('bad req')
+	})
+	it("error with custom fmt caller msg", () => {
+		const [_, err] = fn()
+		const err2 = err.caller((e) => {
+			e.message = `${e.file}:${e.line}`
+			return e
+		})
+
+		expect(err2.code).toBe(Code.BAD_REQUEST)
+		expect(err2.line).toBe("5")
+		expect(err2.func).toBe('fn()')
+		expect(err2.file).toBe(`${__filename}`)
+		expect(err2.message).toBe(`${__filename}:5`)
+	})
+
+	it("error with caller msg", () => {
+		const [_, err] = fn()
+		const err2 = err.caller()
+		console.table(err2)
+		expect(err2.message).toBe(`${__filename}:5 -- bad req`)
 	})
 })
