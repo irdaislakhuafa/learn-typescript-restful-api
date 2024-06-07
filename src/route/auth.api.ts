b8d89d7cd8a6fef4fc0eaac2258733b221f5d145
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-07 15:32:10 +0700

    feat(contact): implement create contact feature
    This commit introduces a new feature that allows users to create contacts.
    It adds a new endpoint to the API that accepts a JSON object with the contact's details, and returns a JSON object with the newly created contact's details.
    
    The endpoint is protected by authentication, and only authenticated users can create contacts.

--- src/route/auth.api.ts
+++ src/route/auth.api.ts
@@ -1,9 +1,14 @@
 import express from "express"
+import { ContactController } from "../business/controller/contact.controller"
 import { UserController } from "../business/controller/user.controller"
 import { middlewareAuth } from "../middleware/auth"
 
 export const authRoute = express.Router()
 authRoute.use(middlewareAuth)
+
+
 authRoute.get("/api/v1/users/current", UserController.getCurrent)
 authRoute.patch("/api/v1/users/current", UserController.updateCurrent)
 authRoute.delete("/api/v1/users/current", UserController.logoutCurrent)
+
+authRoute.post("/api/v1/contacts", ContactController.create)
